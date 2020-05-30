7f5c6ac961ffb7df813070b2bdd5d65c3f93fd68
David Liu <chihweiliu1993@gmail.com>
2020-05-30 10:45:08 +0100

    initial

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styled, { createGlobalStyle, ThemeProvider } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+html {
+  box-sizing: border-box;
+  padding: 0;
+  margin: 0;
+  font-family: 'Nunito', sans-serif;
+}
+*, *:before, *:after {
+  box-sizing: inherit;
+}
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: 'Nunito', sans-serif;
+    color: white;
+  }
+`;
+
+const theme = {
+  primaryColour: "#00404f",
+};
+
+const AppWrapper = styled.div`
+  min-height: 100vh;
+  min-width: 100vw;
+  display: grid;
+  grid-template-rows: max-content max-content max-content;
+  grid-gap: 20px;
+  background: ${(props) => props.theme.primaryColour};
+  justify-items: center;
+  padding: 40px;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <AppWrapper>
+        <GlobalStyle />
+      </AppWrapper>
+    </ThemeProvider>
   );
 }
 
