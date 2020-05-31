a26846de49e162f659b3a9a2cac63c00e275fe0b
David Liu <chihweiliu1993@gmail.com>
2020-05-31 13:27:25 +0100

    cannon styling

--- src/components/Cannon.tsx
+++ src/components/Cannon.tsx
@@ -6,15 +6,38 @@ const CannonWrapper = styled.div<{ screenWidth: number }>`
   position: absolute;
   height: 60px;
   width: 60px;
-  background: grey;
+  background: rgb(128, 128, 128);
+  background: linear-gradient(
+    90deg,
+    rgba(128, 128, 128, 1) 0%,
+    rgba(59, 59, 59, 1) 76%,
+    rgba(56, 56, 56, 1) 100%
+  );
   left: ${(props) => `${props.screenWidth / 2 - 30}px`};
   bottom: 0px;
   border-radius: 50% 50% 0 0;
   z-index: 2;
+  border: 2px solid #4c4c4c;
+`;
+
+const CannonHiglight = styled.div`
+  position: absolute;
+  width: 5px;
+  height: 20px;
+  background: lightgray;
+  bottom: 10px;
+  border-radius: 5px;
+  left: 4px;
 `;
 
 const Barrel = styled.div<{ screenWidth: number; rotation: number }>`
-  background: lightgrey;
+  background: rgb(211, 211, 211);
+  background: linear-gradient(
+    90deg,
+    rgba(211, 211, 211, 1) 0%,
+    rgba(135, 135, 135, 1) 76%,
+    rgba(101, 101, 101, 1) 100%
+  );
   height: 50px;
   width: 20px;
   position: absolute;
@@ -23,6 +46,17 @@ const Barrel = styled.div<{ screenWidth: number; rotation: number }>`
   border-radius: 10% 10% 0 0;
   transform-origin: bottom;
   transform: rotate(${(props) => props.rotation}deg);
+  border: 2px solid gray;
+`;
+
+const BarrelHighlight = styled.div`
+  position: absolute;
+  width: 2px;
+  height: 30px;
+  background: #e2e2e2;
+  bottom: 10px;
+  border-radius: 5px;
+  left: 3px;
 `;
 
 const Laser = styled.div<{ laserLength: number }>`
@@ -64,8 +98,11 @@ const Cannon = () => {
     <>
       <Barrel screenWidth={screenWidth} rotation={cannonRotation}>
         {isCannonFiring && <Laser laserLength={laserLength - meteorSize / 2} />}
+        <BarrelHighlight />
       </Barrel>
-      <CannonWrapper screenWidth={screenWidth}></CannonWrapper>
+      <CannonWrapper screenWidth={screenWidth}>
+        <CannonHiglight />
+      </CannonWrapper>
     </>
   );
 };
